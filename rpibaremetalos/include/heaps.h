539d4bb78580e46cc2afa50e58c054f44499fdea
Stephan Friedl <stephanf01@gmail.com>
2024-12-12 23:51:49 -0700

    Single threaded heaps

--- rpibaremetalos/include/heaps.h
+++ rpibaremetalos/include/heaps.h
@@ -5,6 +5,10 @@
 #pragma once
 
 #include <memory>
+#include "synchronization.h"
+
+static SpinLock __os_static_heap_lock;
+static SpinLock __os_dynamic_heap_lock;
 
 
 extern minstd::single_block_memory_heap &__os_static_heap;
@@ -24,19 +28,25 @@ public:
 template <typename T, typename... Args>
 T *static_new(Args &&...args)
 {
+    LockGuard lock(__os_static_heap_lock);
+
     return new (__os_static_heap.allocate_block<T>(1)) T(minstd::forward<Args>(args)...);
 }
 
 template <typename T, typename... Args>
 inline minstd::unique_ptr<T> make_static_unique(Args &&...args)
 {
-    T *temp = new (__os_static_heap.allocate_block<T>(1)) T(minstd::forward<Args>(args)...);
+     LockGuard lock(__os_static_heap_lock);
+    
+   T *temp = new (__os_static_heap.allocate_block<T>(1)) T(minstd::forward<Args>(args)...);
     return minstd::unique_ptr<T>(temp, __os_static_heap);
 }
 
 template <typename T, typename T2, typename... Args>
 inline minstd::unique_ptr<T2> make_static_unique(Args &&...args)
 {
+    LockGuard lock(__os_static_heap_lock);
+    
     T2 *temp = dynamic_cast<T2*>(new (__os_static_heap.allocate_block<T>(1)) T(minstd::forward<Args>(args)...));
     return minstd::unique_ptr<T2>(temp, __os_static_heap);
 }
@@ -56,18 +66,24 @@ public:
 template <typename T, typename... Args>
 inline minstd::unique_ptr<T> dynamic_new(Args &&...args)
 {
+    LockGuard lock(__os_dynamic_heap_lock);
+    
     return minstd::unique_ptr<T>( new (__os_dynamic_heap.allocate_block<T>(1)) T(minstd::forward<Args>(args)...), __os_dynamic_heap);
 }
 
 template <typename T, typename U, typename... Args>
 inline minstd::unique_ptr<T> dynamic_new(Args &&...args)
 {
+    LockGuard lock(__os_dynamic_heap_lock);
+    
     return minstd::unique_ptr<T>( dynamic_cast<T*>( new (__os_dynamic_heap.allocate_block<U>(1)) U(minstd::forward<Args>(args)...)), __os_dynamic_heap);
 }
 
 template <typename T>
 inline void dynamic_delete(T *pointer)
 {
+    LockGuard lock(__os_dynamic_heap_lock);
+    
     __os_dynamic_heap.deallocate_block(pointer, 1);
 }
 
@@ -75,6 +91,8 @@ inline void dynamic_delete(T *pointer)
 template <typename T, typename... Args>
 inline minstd::unique_ptr<T> make_dynamic_unique(Args &&...args)
 {
+    LockGuard lock(__os_dynamic_heap_lock);
+    
     T *temp = new (__os_dynamic_heap.allocate_block<T>(1)) T(minstd::forward<Args>(args)...);
     return minstd::unique_ptr<T>(temp, __os_dynamic_heap);
 }
@@ -82,6 +100,8 @@ inline minstd::unique_ptr<T> make_dynamic_unique(Args &&...args)
 template <typename T, typename T2, typename... Args>
 inline minstd::unique_ptr<T2> make_dynamic_unique(Args &&...args)
 {
+    LockGuard lock(__os_dynamic_heap_lock);
+    
     T2 *temp = dynamic_cast<T2*>(new (__os_dynamic_heap.allocate_block<T>(1)) T(minstd::forward<Args>(args)...));
     return minstd::unique_ptr<T2>(temp, __os_dynamic_heap);
 }
