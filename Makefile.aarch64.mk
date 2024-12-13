88ed5e0c0422c11adcf8ad12227f376bb97f8540
Stephan Friedl <stephanf01@gmail.com>
2024-12-12 23:49:30 -0700

    Tweaks for QEMU debugging

--- Makefile.aarch64.mk
+++ Makefile.aarch64.mk
@@ -19,11 +19,11 @@ AR := $(GCC_AARCH64_BARE_METAL_TOOLS_PATH)aarch64-none-elf-ar
 OBJCOPY := $(GCC_AARCH64_BARE_METAL_TOOLS_PATH)aarch64-none-elf-objcopy
 CPREPROCESSOR := $(GCC_AARCH64_BARE_METAL_TOOLS_PATH)aarch64-none-elf-cpp
 
-ASM_FLAGS := -Wall -O2 -ffreestanding -mcpu=cortex-a53 -mstrict-align
+ASM_FLAGS := -Wall -Og -ffreestanding -mcpu=cortex-a53 -mstrict-align
 C_CPP_SHARED_FLAGS := -Wall -ffreestanding -fno-stack-protector -nostdinc -nostdlib -nostartfiles -fno-exceptions -fno-unwind-tables -mcpu=cortex-a53 -mstrict-align -fno-threadsafe-statics -mno-outline-atomics
 C_FLAGS := -std=c17 $(C_CPP_SHARED_FLAGS)
 CPP_FLAGS := -std=c++20 -fno-rtti $(C_CPP_SHARED_FLAGS)
-OPTIMIZATION_FLAGS := -Os
+OPTIMIZATION_FLAGS := -Og
 LD_FLAGS := -nostartfiles -nodefaultlibs -nostdlib -static
 
 INCLUDE_DIRS := -I$(GCC_AARCH64_BARE_METAL_INCLUDE)/lib/gcc/aarch64-none-elf/$(GCC_AARCH64_BARE_METAL_VERSION)/include \
